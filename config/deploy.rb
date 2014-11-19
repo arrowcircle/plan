40f2bf003ec6fdb52a93af297a3eff49b4dd9b35
Oleg Bovykin <oleg.bovykin@gmail.com>
2014-11-19 23:04:58 +0300

    Поправил деплой

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,10 +8,10 @@ set :repo_url, 'https://github.com/arrowcircle/plan.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/webmaster/projects/production/plan'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -23,10 +23,10 @@ set :repo_url, 'https://github.com/arrowcircle/plan.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
