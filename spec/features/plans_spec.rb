5e82116aa2dd192a4f0b0250ac23b3cc26a487b8
Bogdan <b.orlov.fl@yandex.ru>
2014-12-12 11:45:44 +0300

    plans_tests

---
+++ spec/features/plans_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+require 'support/user_macros'
+
+feature 'Планы' do
+  before(:each) { login }
+
+  scenario 'добавляет план' do
+    visit plans_path
+    click_link I18n.t('new'), match: :first
+    name = Faker::Product.product_name
+    fill_in :plan_name, with: name
+    click_button I18n.t('save')
+    expect(page).to have_content name
+    expect(page).to have_content 'План добавлен'
+  end
+  scenario 'удаляет план' do
+    visit plans_path
+    click_link I18n.t('delete'), match: :first
+    expect { click_button I18n.t('delete'), match: :first }.to change(Plan, :count).by(1)
+  end
+  scenario "управление планами" do
+    let(:root) { create(:root) }
+    let(:parent) { create(:item, account_id: root.account_id) }
+    let(:item) { create(:item, account_id: root.account_id) }
+    let(:subitem) { create(:item, account_id: root.account_id) }
+    Itemization.create(parent_id: parent.id, item_id: item.id, quantity: 1, account_id: parent.account_id)
+    Itemization.create(parent_id: parent.id, item_id: subitem.id, quantity: 2, account_id: parent.account_id)
+    it 'добавление изделия в план' do
+      login
+      visit plans_path
+      click_link I18n.t('new'), match: :first
+      name = Faker::Product.product_name
+      fill_in :plan_name, with: name
+      click_link I18n.t('add_item'), match: :first
+      fill_in :item_name, with: parent.articul
+      fill_in :quantity, with: 2
+      click_button I18n.t('save')
+      expect(page).to have_content name
+    end
+    it 'просмотр плана' do
+      login
+      visit plans_path
+      click_link I18n.t('show'), match: :first
+      expect(page).to have_content item.name
+      expect(page).to have_content subitem.name
+    end
+
+  end
+end
